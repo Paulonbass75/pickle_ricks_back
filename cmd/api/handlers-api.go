ee7e2b55419c4de19cf58a6ec04115053cbc3080
Paulonbass75 <paulmpool@yahoo.com>
2024-03-05 07:00:47 -0500

    Working succesful trnsaction page

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -14,7 +14,7 @@ type stripePayload struct {
 
 type jsonResponse struct {
 	OK      bool   `json:"ok"`
-	Message     string `json:"message,omitempty"`
+	Message string `json:"message,omitempty"`
 	Content string `json:"content,omitempty"`
 	ID      int    `json:"id,omitempty"`
 }
--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,3 +7,31 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		app.errorlog.Println(err)
 	}
 }
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	// Assume that the payment was successful
+	err := r.ParseForm()
+	if err != nil {
+		app.errorlog.Println(err)
+	}
+	// read posted data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("amount")
+	paymentCurrency := r.Form.Get("payment_Currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["payment_intent"] = paymentIntent
+	data["payment_method"] = paymentMethod
+	data["payment_Amount"] = paymentAmount
+	data["payment_Currency"] = paymentCurrency
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorlog.Println(err)
+	}
+}
